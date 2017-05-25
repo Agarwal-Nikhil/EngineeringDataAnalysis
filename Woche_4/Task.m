078e0dc750543831912ce5b83d5aecd8448db52e
a-parida12 <abhijeet.parida@tum.de>
2017-05-25 11:27:13 +0200

    message

---
+++ Woche_4/Task.m
@@ -0,0 +1,33 @@
+clc
+clear all;
+load 'Timber.dat';
+E = Timber(1:50,5); %YoungsModulus, N/mm^2
+P = 2000; %N
+L = 4000; %mm
+b = 150; %mm
+h = 250; %mm
+I = b*(h^3)/12;
+
+E
+%Fitting logn to mu and var
+param = lognfit(E);
+
+mu=param(1);
+var=param(2);
+
+%create seeds for the montecarlo sims
+seed=normrnd(mu*1000,var*1000,1,1000);
+
+u = P*(L^3)./(3*seed*I);
+
+figure(1)
+subplot(1,2,1)
+hist(u,10)
+title('Histogram diplacement')
+subplot(1,2,2)
+cdfplot(u)
+title('CDF')
+
+figure(2)
+
+
