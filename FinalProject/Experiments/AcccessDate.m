91ae7220dd10421eca989595d17b23ecc1b8df03
Nikhil <nikhil.agarwal.globetrotter@gmail.com>
2017-06-30 17:07:05 +0200

    Almost Done

--- FinalProject/Experiments/AcccessDate.m
+++
@@ -1,7 +0,0 @@
-clear
-clc
-start = datetime('now');
-vec = start + hours(0:24).'; % datetime + duration = datetime
-midnight = dateshift(start, 'end', 'day');
-pastMidnight = vec > midnight; % relational operators work
-T = table(vec, pastMidnight)
--- FinalProject/Experiments/DateTime.m
+++
@@ -1,6 +0,0 @@
-clear;
-clc;
-t = datetime('now') + calyears(0:2) + calmonths(0:2) + hours(20:20:60);
-if t.Year == 2017
-    print t.Month
-end
